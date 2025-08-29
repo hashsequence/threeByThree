4e4d3a2c1d81b598fc8725262c533f92c80dfe3e
Avery Wong <awong@intrinsic.ventures>
2025-08-28 23:47:06 -0700

    update to rotate any square matrix

--- main.go
+++ main.go
@@ -1,5 +1,3 @@
-
-
 package main
 
 import (
@@ -8,6 +6,107 @@ import (
     "os"
     "strconv"
 )
+
+// rotateSquareMatrixCounterClockwise rotates every square submatrix of nonempty cells counterclockwise 90 degrees
+func rotateSquareMatrixCounterClockwise(records [][]string) [][]string {
+    rows := len(records)
+    if rows == 0 {
+        return records
+    }
+    cols := len(records[0])
+    minDim := rows
+    if cols < rows {
+        minDim = cols
+    }
+    // For every possible size (2x2 up to minDim x minDim)
+    for size := 2; size <= minDim; size++ {
+        for i := 0; i <= rows-size; i++ {
+            for j := 0; j <= cols-size; j++ {
+                // Check if all cells in the submatrix are nonempty
+                allNonEmpty := true
+                for r := i; r < i+size; r++ {
+                    for c := j; c < j+size; c++ {
+                        if records[r][c] == "" {
+                            allNonEmpty = false
+                            break
+                        }
+                    }
+                    if !allNonEmpty {
+                        break
+                    }
+                }
+                if !allNonEmpty {
+                    continue
+                }
+                // Check if the submatrix is surrounded by empty cells or the border
+                surrounded := true
+                // Top border
+                if i > 0 {
+                    for c := j; c < j+size; c++ {
+                        if records[i-1][c] != "" {
+                            surrounded = false
+                            break
+                        }
+                    }
+                }
+                // Bottom border
+                if surrounded && i+size < rows {
+                    for c := j; c < j+size; c++ {
+                        if records[i+size][c] != "" {
+                            surrounded = false
+                            break
+                        }
+                    }
+                }
+                // Left border
+                if surrounded && j > 0 {
+                    for r := i; r < i+size; r++ {
+                        if records[r][j-1] != "" {
+                            surrounded = false
+                            break
+                        }
+                    }
+                }
+                // Right border
+                if surrounded && j+size < cols {
+                    for r := i; r < i+size; r++ {
+                        if records[r][j+size] != "" {
+                            surrounded = false
+                            break
+                        }
+                    }
+                }
+                if surrounded {
+                    // Rotate the submatrix counterclockwise
+                    rotateSubMatrixCCW(records, i, j, size)
+                }
+            }
+        }
+    }
+    return records
+}
+
+// rotateSubMatrixCCW rotates a square submatrix in place
+func rotateSubMatrixCCW(records [][]string, startRow, startCol, size int) {
+	// Layer by layer rotation
+	for layer := 0; layer < size/2; layer++ {
+		first := layer
+		last := size - 1 - layer
+		for k := first; k < last; k++ {
+			offset := k - first
+			// Save top
+			top := records[startRow+first][startCol+k]
+			// right -> top
+			records[startRow+first][startCol+k] = records[startRow+k][startCol+last]
+			// bottom -> right
+			records[startRow+k][startCol+last] = records[startRow+last][startCol+last-offset]
+			// left -> bottom
+			records[startRow+last][startCol+last-offset] = records[startRow+last-offset][startCol+first]
+			// top -> left
+			records[startRow+last-offset][startCol+first] = top
+		}
+	}
+}
 
 // rotate3x3CounterClockwise rotates each 3x3 matrix counterclockwise around the center cell if it is fully surrounded
 func rotate3x3CounterClockwise(records [][]string) [][]string {
